0b785151b37964e7bb2764df0111e0fe1cdf5786
Lior Roife <72598478+LiorRoife@users.noreply.github.com>
2023-05-12 21:39:28 +0300

    Update UserInterface.cs

--- UserInterface.cs
+++ UserInterface.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -32,23 +32,29 @@ namespace Reversed_TicTacToe_For_Console
         //public GameLogics GameLogics;
         public static void Run()
         {
+            GameLogics Game;
             int boardSize;
-            int players;
+            int amountOfPlayers;
+            string validMove;
+
             Console.WriteLine("Welcome to Tic-Tac-Toe!");
-            boardSize= GetGameBoardSizeInput();
-            players= GetAmountOfPlayers();
-
-            GameLogics Game= new GameLogics()
-            //set logics: BoardSize , PVP?
-            int userInputBoardSize = 0;
-            int userInputNumberOfPlayers = 0;
-            string userInput = Console.ReadLine();
-            UserInterface.
-            while (CheckBoardSizeValidity(userInput) != true)
+            boardSize = GetGameBoardSizeInput();
+            amountOfPlayers = GetAmountOfPlayers();
+            if (amountOfPlayers == 1)
             {
-                userInput = Console.ReadLine();
+                Game = new GameLogics(boardSize, true);//1 player
             }
+            else
+            {
+                Game = new GameLogics(boardSize, false);//2 players
+            }
+            Game.m_CurrentGameBoard.PrintBoard();
 
+            validMove = getPlayerMoveInput(Game);
+            ExecuteValidMove(validMove);
+
+            Console.WriteLine("testttttt ");
+            Console.WriteLine("test ");
 
         }
 
@@ -59,8 +65,9 @@ namespace Reversed_TicTacToe_For_Console
 
             Console.WriteLine("Please enter the size of the game board (the board is square shaped and its' size must be between 3 to 9):");
             inputString = Console.ReadLine();
-            while (CheckBoardSizeValidity(inputString) != true)
+            while (GameLogics.CheckBoardSizeValidity(inputString) != true)
             {
+                Console.WriteLine("Invalid input, please enter a number between 3 to 9");
                 inputString = Console.ReadLine();
             }
             userInputBoardSize = int.Parse(inputString);
@@ -68,100 +75,39 @@ namespace Reversed_TicTacToe_For_Console
             return userInputBoardSize;
         }
 
-        public static bool CheckBoardSizeValidity(string i_InputStringFromUser)
-        {
-            bool isValidInput = false;
-            if (i_InputStringFromUser.All(char.IsDigit) == true)
-            {
-                if (int.Parse(i_InputStringFromUser) > 2 && int.Parse(i_InputStringFromUser) < 10)
-                {
-                    isValidInput = true;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input, please enter a number between 3-9");
-                    isValidInput = false;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid input, please enter a number");
-                isValidInput= false;
-            }
-
-            return isValidInput;
-        }
-
         public static int GetAmountOfPlayers()
         {
             int userInputAmountPlayers = 0;
             string inputString;
 
-            Console.WriteLine("Please enter the amount of players (1 for Palyer VS Computer , 2 For Player VS Player:");
+            Console.WriteLine("Please enter the amount of players (1 for Palyer VS Computer , 2 for Player VS Player:");
             inputString = Console.ReadLine();
-            while (CheckAmountOfPlayersValidity(inputString) != true)
+            while (GameLogics.CheckAmountOfPlayersValidity(inputString) != true)
             {
+                Console.WriteLine("Invalid input, please enter a number between 1 to 2");
                 inputString = Console.ReadLine();
             }
             userInputAmountPlayers = int.Parse(inputString);
-           // ticTacToeLogic = new GameLogic(boardSizeUserInput, numOfHumanPlayersUserInput);
             return userInputAmountPlayers;
         }
 
-        public static bool CheckAmountOfPlayersValidity(string i_InputStringFromUser)
+        private static string getPlayerMoveInput(GameLogics io_CurrentGame)
         {
-            bool isValidInput = false;
+            string inputString;
 
-            if (i_InputStringFromUser.All(char.IsDigit) == true)
-            {
-                if (int.Parse(i_InputStringFromUser) > 0 && int.Parse(i_InputStringFromUser) < 3)
-                {
-                    isValidInput = true;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input, please enter a number");
-                    isValidInput= false;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid input, please enter a number");
-                isValidInput= false;
-            }
+            Console.WriteLine("Please enter your next move in the format of row,column or 'Q' to quit.", Environment.NewLine);
+            inputString = Console.ReadLine();
 
-            return isValidInput;
+            while (GameLogics.CheckMoveValidity(inputString, io_CurrentGame) != true)
+            {
+                Console.WriteLine("Invalid move input, please make sure you choose empty cell or 'Q' to quit.");
+                inputString = Console.ReadLine();
+            }
+            // if player press q so go to EndGame
+            // getPlayerMove(io_CurrentGame);
+            return inputString;
         }
+
+  
     }
 }
-
-
-/*
-            Screen.Clear();
-            //Print first row of number.
-            Console.Write("  ");
-            for (int colNum = 0; colNum < 5; colNum++)
-            {
-                Console.Write("{0}   ", colNum + 1);
-            }
-            Console.WriteLine();
-            //Print each row.
-            for (int rowNum = 0; rowNum < 5; rowNum++)
-            {
-                Console.Write("{0}|", rowNum + 1);
-                for (int colNum = 0; colNum < 5; colNum++)
-                {
-                    Console.Write(" {0} |", " "); // board[x,y]
-                }
-                Console.WriteLine();
-                Console.Write(" ");
-                for (int colNum = 0; colNum < (5 * 4) + 1; colNum++)
-                {
-                    Console.Write("=");
-                }
-                Console.WriteLine();
-            }
-
-            Console.WriteLine("enter size");
-            size = Console.ReadLine();
-            */
